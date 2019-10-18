aa0b4d9678fe675be6808fd2ac7359dfbb024032
JakeTidiman <41200319+JakeTidiman@users.noreply.github.com>
2019-10-18 10:36:23 +0100

    working on HUD

--- TinyGame2/Assets/JakeTidiman/JT_Scripts/CanvasSway.cs
+++ TinyGame2/Assets/JakeTidiman/JT_Scripts/CanvasSway.cs
@@ -24,7 +24,7 @@ public class CanvasSway : MonoBehaviour
 
     public void MoveUI()
     {
-        if (Input.GetKey(KeyCode.A) && uiElements.transform.localPosition.x < 30)
+        if (Input.GetKey(KeyCode.A) && uiElements.transform.localPosition.x < 20)
         {
             uiElements.transform.Translate(Time.deltaTime * 50, 0, 0);
         }
@@ -33,7 +33,7 @@ public class CanvasSway : MonoBehaviour
             timer = 0;
         }
 
-        if (Input.GetKey(KeyCode.D) && uiElements.transform.localPosition.x > -30)
+        if (Input.GetKey(KeyCode.D) && uiElements.transform.localPosition.x > -20)
         {
             uiElements.transform.Translate(Time.deltaTime * -50, 0, 0);
         }
@@ -42,7 +42,7 @@ public class CanvasSway : MonoBehaviour
             timer = 0;
         }
 
-        if (!Input.anyKey)
+        if (!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
         {
             timer += Time.deltaTime * 5;
             uiElements.transform.position = Vector3.Lerp(uiElements.transform.position, transform.position, timer);
